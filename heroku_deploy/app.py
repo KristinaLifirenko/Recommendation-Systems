27b2d21f43811bb7cd5c86ec9f671775f66ac248
KristinaLifirenko <58210448+KristinaLifirenko@users.noreply.github.com>
2020-06-07 01:19:32 +0300

    app.py

--- heroku_deploy/app.py
+++ heroku_deploy/app.py
@@ -8,7 +8,7 @@ import time
 import requests
 from ast import literal_eval
 
-@st.cache
+#@st.cache
 def load_embeddings():
 
     """Функция для загрузки векторных представлений"""
@@ -21,8 +21,9 @@ def load_embeddings():
     return item_embeddings,nms_idx
 
 
+#@st.cache(allow_output_mutation=True)
 @st.cache
-def nearest_items_nms(itemid, index, n=5):
+def nearest_items_nms(itemid, index, n=10):
 
     """Функция для поиска n ближайших соседей, возвращает построенный индекс"""
 
@@ -30,14 +31,17 @@ def nearest_items_nms(itemid, index, n=5):
     nn = index.knnQuery(item_embeddings[itemid], k=n)
     return nn
 
-@st.cache
 def read_files():
     """
     Функция для работы с продуктовым словарем
     """
     items = pd.read_csv('food_dict_prepared.csv')
+    items['itemid'] = items['itemid'].astype(str)
+    items['price'].fillna('Not Available',inplace=True)
+    items['image'].fillna('No Image Available',inplace=True)
     items['image'] = items['image'].apply(lambda x: x[2:-2] if pd.notnull(x) else x)
-    #items['style'] = items['style'].fillna('No description')
+    items['description'] = items['description'].apply(lambda x: x[2:-2] if pd.notnull(x) else x)
+
     return items
 
 @st.cache
@@ -46,15 +50,18 @@ def product_description(option):
     Функция для создания составляющих описания товара
     """
     choice = products[products['itemid'] == option]
+    try:
+        url = choice['image'].iloc[0]
+        img = requests.get(url).content
+    except:
+        img = 'No Image Available'
+    price = choice['price'].iloc[0]
     title = choice['title'].iloc[0]
- #   url = choice['image'].iloc[0]
- #   img = requests.get(url).content
- #   style = choice['style'].iloc[0]
- #   rating = choice['rating'].mean()
- #   rating_func = lambda r: {r<0.2 : 1, 0.2<r<0.4 : 2, 0.4<r<0.6 : 3, 0.6<r<0.8 : 4, r>0.8 : 5}[1]
- #   rating_star = rating_func(rating) 
-   # return choice, img, style, rating_star
-    return choise, title
+    brand = choice['brand'].iloc[0]
+    desc = choice['description'].iloc[0]
+    rank = choice['rank'].iloc[0]
+
+    return choice, img, price, title, brand, desc, rank
     
 
 st.title('Welcome to Recommendation System Prototype')
@@ -67,58 +74,39 @@ item_embeddings,nms_idx = load_embeddings()
 product = st.text_input('Search for product...', '')
 
 # находим все подходящие товары в датасете
-#output = products[products['itemid'].str.contains(product) >0]
-#output = products[products['itemid'].str.contains(product)>=0]
-output = products[products['itemid'] == product]
+output = products[products['itemid'].str.contains(product)]
 
 # выбираем товар из списка
 option = st.selectbox('Select product', output['itemid'].values)
 
 # выводим информацию о выбранном товаре
-st.header('Product info: ')
-#choice, img, style, rating_star = product_description(option)
-choise, title = product_description(option)
-#st.image(img, width=150)  # картинка
+st.header('Product info:')
+choice, img, price, title,brand,desc, rank = product_description(option)
+st.image(img, width=150)  # картинка
+st.markdown(f'**{price}**')
 st.markdown(f'*{title}*')
-#if style != 'No description':  # описание
-#    style = literal_eval(style)
-#    for key, value in style.items():
-#        st.markdown(f'*{key}*')
-#        st.write(value)
-#else:
-#    st.text(style)
-#st.markdown(':star:'*rating_star) # рейтинг товара
+st.markdown(f'*{brand}*')
+st.markdown(desc)
 
-# также можно посмотреть отзывы покупателей об этом товаре
-#if st.button("Show customers' reviews on the product"):
-#    rec = choice.drop_duplicates('reviewerName')
-#    for name, summary, review in zip(rec['reviewerName'][:5], rec['summary'][:5], rec['reviewText'][:5]): 
-#        st.subheader(name)
-#        st.markdown(f'----*{summary}*----')
-#        st.write(review)
-#        st.markdown('------')
-
-# а тут рекомендации к товару
+# выводим на боковую панель рекомендации к товару
 st.sidebar.header('Products, you may also like: ')
+
 #Ищем рекомендации
+output = products[products['itemid'] == option]
 val_index = output[output['itemid'].values == option]['itemid'].iloc[0]
 index = nearest_items_nms(val_index, nms_idx, 5)
 
-#Выводим рекомендации с краткой информацией о товаре
+#Выводим для каждой рекомендации инфо о товаре
 for idx in index[0][1:]:
     try:
-#       choice, img, style, rating_star = product_description(str(idx))
-        choise, title = product_description(option)
-        #st.sidebar.image(img, caption=f'Product ID: {idx}') # картинка
+        choice, img, price, title,brand,desc, rank = product_description(str(idx))
+        if img !='No Image Available':
+            st.sidebar.image(img, caption=f'Product ID: {idx}') # картинка
+        else:
+            st.sidebar.markdown('-----')
+
         st.sidebar.markdown(f'*{title}*')
-#        if style != 'No description':  # описание
-#            style = literal_eval(style)
-#            for key, value in style.items():
-#                st.sidebar.text(key)
-#                st.sidebar.text(value)
-#        else:
-#             st.sidebar.text(style)
-#        st.sidebar.markdown(':star:'*rating_star) # рейтинг
+        st.sidebar.markdown(f'**{price}**')
         st.sidebar.markdown('-----')
     except:
         st.sidebar.text('')
