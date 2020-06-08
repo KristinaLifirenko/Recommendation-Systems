29e7710c649fa36c1bce7aa7432e38bcc55372ea
KristinaLifirenko <58210448+KristinaLifirenko@users.noreply.github.com>
2020-06-08 13:35:50 +0300

    Add files via upload

--- heroku_deploy/app.py
+++ heroku_deploy/app.py
@@ -8,7 +8,7 @@ import time
 import requests
 from ast import literal_eval
 
-#@st.cache
+#@st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def load_embeddings():
 
     """Функция для загрузки векторных представлений"""
@@ -21,8 +21,7 @@ def load_embeddings():
     return item_embeddings,nms_idx
 
 
-#@st.cache(allow_output_mutation=True)
-@st.cache
+#@st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def nearest_items_nms(itemid, index, n=10):
 
     """Функция для поиска n ближайших соседей, возвращает построенный индекс"""
@@ -31,7 +30,7 @@ def nearest_items_nms(itemid, index, n=10):
     nn = index.knnQuery(item_embeddings[itemid], k=n)
     return nn
 
-def read_files():
+def read_files(folder_name='data'):
     """
     Функция для работы с продуктовым словарем
     """
@@ -44,7 +43,17 @@ def read_files():
 
     return items
 
-@st.cache
+def read_reviews(folder_name='data'):
+    """
+    Функция для чтения файла с отзывами
+    """
+    items = pd.read_csv('image.csv')
+    items['itemid'] = items['itemid'].astype(str)
+    items['image'] = items['image'].apply(lambda x: x[2:-2] if pd.notnull(x) else x)
+    items['style'] = items['style'].fillna('No description')
+    return items
+
+#@st.cache
 def product_description(option):
     """
     Функция для создания составляющих описания товара
@@ -62,12 +71,11 @@ def product_description(option):
     rank = choice['rank'].iloc[0]
 
     return choice, img, price, title, brand, desc, rank
-    
 
 st.title('Welcome to Recommendation System Prototype')
 
 #Загружаем данные
-products  = read_files()  
+products = read_files()
 item_embeddings,nms_idx = load_embeddings()
 
 # вводим id или часть id интересующего нас товара
@@ -81,6 +89,18 @@ option = st.selectbox('Select product', output['itemid'].values)
 
 # выводим информацию о выбранном товаре
 st.header('Product info:')
+
+#можно посмотреть отзывы покупателей об этом товаре
+if st.checkbox("Show customers' reviews on the product"):
+    reviews = read_reviews()
+    ch = reviews[reviews['itemid'] == option]
+    rec = ch.drop_duplicates('reviewerName')
+    for name, summary, review in zip(rec['reviewerName'][:5], rec['summary'][:5], rec['reviewText'][:5]): 
+        st.subheader(name)
+        st.markdown(f'----*{summary}*----')
+        st.write(review)
+        st.markdown('------')
+
 choice, img, price, title,brand,desc, rank = product_description(option)
 st.image(img, width=150)  # картинка
 st.markdown(f'**{price}**')
@@ -96,15 +116,18 @@ output = products[products['itemid'] == option]
 val_index = output[output['itemid'].values == option]['itemid'].iloc[0]
 index = nearest_items_nms(val_index, nms_idx, 5)
 
+
 #Выводим для каждой рекомендации инфо о товаре
 for idx in index[0][1:]:
     try:
         choice, img, price, title,brand,desc, rank = product_description(str(idx))
-        if img !='No Image Available':
+        try:
+# if img !='No Image Available':
             st.sidebar.image(img, caption=f'Product ID: {idx}') # картинка
-        else:
-            st.sidebar.markdown('-----')
-
+#        else:
+        except:
+            st.sidebar.markdown('No Image')
+            
         st.sidebar.markdown(f'*{title}*')
         st.sidebar.markdown(f'**{price}**')
         st.sidebar.markdown('-----')
